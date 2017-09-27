aeac9801c0f4809b515160565a0333719d14f3f1
dailinyang <dailinyang@gmail.com>
2017-09-27 22:24:26 +0800

    v0.2
    1. decode dynamically.
    2. rewrite decode code.
    3. t1.py change output to array string for MU.

--- t1.py
+++ t1.py
@@ -6,43 +6,48 @@ WHITE = 255
 BLACK = 0
 point_size = 16
 font = ImageFont.truetype("msjhl.ttc", point_size)
+#font = ImageFont.truetype("arial.ttf", point_size)
 outary = []
 
-for char in '峰':
+print("[")
+for char in 'Taiwanbit伸峰':
     im = Image.Image()._new(font.getmask(char,mode='1'))
     a= im.resize((16,16))
     for x in range(0,16):
         for y in range(0,16):
-            print(str(a.getpixel((y,x)))+",",end="")
-        print()
+            pass
+            #print(str(a.getpixel((y,x)))+",",end="")
+        #print()
 	#print("\n")
-    im.save(char + ".bmp")
+    #im.save(char + ".bmp")
+    a.save(char + ".bmp")
     
 
-x = 0
-y = 0
-for y in range(16):
-    
-    x=0
-    turn = WHITE
-    count = 0
-    while x<16:    
-        #print(str(x))
-        if a.getpixel((15-x,y)) == turn:
-            count+=1
-            x+=1
+    x = 0
+    y = 0
+    outary.clear()
+    for y in range(16):
+        x=0
+        turn = WHITE
+        count = 0
+        while x<16:    
+            #print(str(x))
+            if a.getpixel((15-x,y)) == turn:
+                count+=1
+                x+=1
+            else:
+                outary.append(count)
+                if turn == WHITE:
+                    turn = BLACK
+                else:
+                    turn = WHITE
+                count=1
+                x+=1
         else:
             outary.append(count)
-            if turn == WHITE:
-                turn = BLACK
-            else:
-                turn = WHITE
-            count=1
-            x+=1
-    else:
-        outary.append(count)
-            
-        
-print( "".join(str(i)+"," for i in outary)[:-1]  )
+                
+    print("'",end="")        
+    print( "".join(str(i)+"," for i in outary)[:-1],end=""  )
     
-    
+    print("',\\")
+print("]")
